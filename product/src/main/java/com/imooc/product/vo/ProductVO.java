41e867aae192334c2f5f227f09776a56fbf67a0d
wangbo <wangbo@myttpl.com>
2019-01-13 23:08:33 +0800

    ss

---
+++ product/src/main/java/com/imooc/product/vo/ProductVO.java
@@ -0,0 +1,20 @@
+package com.imooc.vo;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class ProductVO {
+
+    @JsonProperty(value = "name")
+    private String categoryName;
+
+    @JsonProperty(value = "type")
+    private Integer categoryType;
+
+    @JsonProperty(value = "foods")
+    private List<ProductInfoVO> productInfoVOList;
+
+}
